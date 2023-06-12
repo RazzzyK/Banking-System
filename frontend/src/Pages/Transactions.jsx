1d547f450c05b2e3ca4609f3c9bf00497bf6ab66
RazzzyK <Razenok@yahoo.com>
2023-06-12 18:17:02 -0400

    Added skeleton for transaction table, minor cleanup/changes

--- frontend/src/Pages/Transactions.jsx
+++ frontend/src/Pages/Transactions.jsx
@@ -1,9 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import '../CSS/Log-RegFormStyle.css';
+import '../CSS/TransactionsStyle.css';
+import { useTable } from 'react-table';
+import DataTable from 'react-data-table-component';
 
 
 export const Transactions = (props) => {
+    // const [data, useState] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [perPage, setPerPage] = useState(10)
+
+    const columns = [
+        // {
+        //     name: "ID",
+        //     selector: (row) = row.id
+        // },
+        {
+            name: "Transaction Type",
+            selector: row => row.title,
+            sortable: true,
+        },
+        {
+            name: "Amount",
+            selector: row => row.year,
+            sortable: true,
+        },
+    ]
+
+    const data = [
+        {
+            id: 1,
+            title: 'Beetlejuice',
+            year: '1988',
+        },
+        {
+            id: 2,
+            title: 'Ghostbusters',
+            year: '1984',
+        },
+    ]
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,8 +46,17 @@ export const Transactions = (props) => {
     }
 
     return (
-        <div className="">
-            <h2>Transactions</h2>
+        <div className="page">
+            <div className="table">
+                <DataTable
+                    title="Data"
+                    columns={columns}
+                    data={data}
+                    progressPending={loading}
+                    pagination
+                    selectableRows
+                />
+            </div>
         </div>
     )
 }
